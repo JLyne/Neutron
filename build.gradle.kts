a335312a14d7ea16f4954be6ede44d30a718be20
James Lyne <jim+github@not-null.co.uk>
2025-10-09 19:44:19 +0100

    Java 25

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ description = "An open-source essential suite for the Velocity proxy"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
+        languageVersion = JavaLanguageVersion.of(25)
     }
 }
 
